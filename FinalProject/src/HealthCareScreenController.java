edbcac3f6f0f62b18de4b4183a03436ce0e9282e
blepsch <blepsch@uiowa.edu>
2021-12-04 07:09:18 -0600

    Comment

--- FinalProject/src/HealthCareScreenController.java
+++ FinalProject/src/HealthCareScreenController.java
@@ -25,3 +25,4 @@ public class HealthCareScreenController {
     public void backArrow(MouseEvent mouseEvent) {
     }
 }
+//Ben1 abcd
--- FinalProject/src/LoginController.java
+++ FinalProject/src/LoginController.java
@@ -143,3 +143,4 @@ public class LoginController {
         stage.show();
     }
 }
+
