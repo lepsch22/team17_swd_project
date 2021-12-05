67d8d2811f50a6b7e5d7df73f1f95cb2eb600499
blepsch <blepsch@uiowa.edu>
2021-12-04 18:47:19 -0600

    Javadocs

--- FinalProject/src/OrganizationInfoController.java
+++ FinalProject/src/OrganizationInfoController.java
@@ -17,13 +17,30 @@ import java.sql.SQLException;
 import java.util.HashMap;
 
 public class OrganizationInfoController {
-    public ImageView orgIcon;
-    public Label orgName;
-    public TextArea requirementTextArea;
+    /**
+     * orgIcon image
+     */
+    @FXML
+    private ImageView orgIcon;
+    /**
+     * name of org
+     */
+    @FXML
+    private Label orgName;
+    /**
+     * Text area for mask requirements
+     */
+    @FXML
+    private TextArea requirementTextArea;
     private String nameOfOrg;
     private String requirementText;
 
-
+    /**
+     * SetInf from previous controller
+     * @param nameOfOrg name of org
+     * @throws SQLException exception
+     * @throws NoSuchAlgorithmException bad thing happened
+     */
     public void setInfo(String nameOfOrg ) throws SQLException, NoSuchAlgorithmException {
         orgName.setText(nameOfOrg);
         ResultSet rs = Database.getRegulation(orgName.getText());
@@ -31,16 +48,17 @@ public class OrganizationInfoController {
         requirementTextArea.appendText(rs.getString("Regulations"));
     }
 
+    /**
+     * Go back to previous window
+     * @param mouseEvent on mouse click
+     * @throws IOException couldnt load fxml
+     */
     public void backArrow(MouseEvent mouseEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/UserScreen.fxml"));
         Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-    }
-    @FXML
-    public void initialize() throws SQLException, NoSuchAlgorithmException {
-
     }
 
 }
