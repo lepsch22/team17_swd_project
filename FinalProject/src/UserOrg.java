caab51a2b9c7906026acddbd1724e4bb489f38c7
blepsch <blepsch@uiowa.edu>
2021-12-05 06:37:07 -0600

    styling *kissyface*

---
+++ FinalProject/src/UserOrg.java
@@ -0,0 +1,39 @@
+public class UserOrg {
+    private String userName;
+
+    public String getUser() {
+        return userName;
+    }
+
+    public void setUser(String user) {
+        this.userName = user;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    private String orgName;
+    private  String location;
+
+    public UserOrg(String user, String orgName, String location) {
+        this.userName = user;
+        this.orgName = orgName;
+        this.location = location;
+    }
+
+
+    
+}
